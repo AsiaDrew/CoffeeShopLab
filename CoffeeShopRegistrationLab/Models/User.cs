ed4f3d15e3b89e743aa6bacb7a0f49792605e3c1
AsiaDrew <asiadrew2@outlook.com>
2022-07-22 09:06:40 -0400

    first

---
+++ CoffeeShopRegistrationLab/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeeShopRegistrationLab.Models
+{
+    public class User
+    {
+        //The form should take in the user’s first and last name, email, and password.
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
